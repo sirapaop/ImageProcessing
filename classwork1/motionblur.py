a7505bb647a55cfeb38094c9a6235dca53370e94
sirapaop <sirapaop2545@gmail.com>
2023-07-09 13:41:20 +0700

    classwork1

---
+++ classwork1/motionblur.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('classwork1/bot.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.resize(img,(600,600))
+
+size = 20
+
+filter_motionblur = np.zeros((size, size))
+# vertical
+#filter_motionblur[:, int((size - 1)/2)] = np.ones(size) 
+
+#horicontal
+#filter_motionblur[int((size-1)/2), :] = np.ones(size)
+
+#ทะแยงซ้ายไปขวา
+#filter_motionblur = np.eye(20, dtype=np.float32) 
+
+#ทะแยงขวาไปซ้าย
+for i in range(size):
+  filter_motionblur[i, size - 1 - i] =  1  
+
+filter_motionblur = filter_motionblur / size
+print(filter_motionblur)
+output = cv.filter2D(img, -1, filter_motionblur)
+
+#cv.imwrite('filter/motion_output.png', output)
+cv.imshow('Motion_Blur Image', output)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+
+
