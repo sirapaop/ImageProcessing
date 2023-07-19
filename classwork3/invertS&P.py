01be951052ab2d3491572790915c8a6cc66151c4
sirapaop <sirapaop2545@gmail.com>
2023-07-19 09:35:38 +0700

    fourier

--- classwork3/invertS&P.py
+++ classwork3/invertS&P.py
@@ -1,15 +1,13 @@
 import cv2 as cv
 import numpy as np 
 
-
-
 img = cv.imread("classwork3\salt&pepper_Image.jpg", cv.IMREAD_GRAYSCALE)
 
-denoised_image = cv.medianBlur(img, 5)  
+for i in range(3,13,2):
+    denoised_image = cv.medianBlur(img, i) 
+    print(i) 
+    cv.imshow('invert_Image', denoised_image)
 
-#cv2.imwrite('output_image.png', denoised_image)
-
-
-cv.imshow('invert_Image', denoised_image)
+cv.imshow('P&S_Image', img)  
 cv.waitKey(0)
 cv.destroyAllWindows()
